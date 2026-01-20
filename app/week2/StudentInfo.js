ad690ed0ed94287d5b488a0aa2fe96821d5ca0f4
ayush <100313112+12catsk@users.noreply.github.com>
2026-01-20 13:48:29 -0700

    Initial CPRG 306 project with Week 2

---
+++ app/week2/StudentInfo.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <section>
+      <p>Ayush Sainju</p>
+      <p>
+        <Link href="https://github.com/YOUR_GITHUB_USERNAMEcprg306-assignments">
+          https://github.com/YOUR_GITHUB_USERNAME/cprg306-assignments
+        </Link>
+      </p>
+    </section>
+  );
+}
