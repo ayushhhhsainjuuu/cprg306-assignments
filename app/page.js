d1cebd9d879785777f51e7dc96fc6275ba8b72ef
ayush <100313112+12catsk@users.noreply.github.com>
2026-02-05 09:03:00 -0700

    bugfix: correct typo in page title

--- app/page.js
+++ app/page.js
@@ -9,13 +9,19 @@ export default function Home() {
 
       <ul className="list-disc pl-6 space-y-2">
         <li>
-          <Link className="text-blue-600 hover:underline" href="/week-2">
+          <Link href="/week-2" className="text-blue-600 hover:underline">
             Week 2 - Shopping List
           </Link>
         </li>
 
         <li>
-          <Link className="text-blue-600 hover:underline" href="/week-4">
+          <Link href="/week-3" className="text-blue-600 hover:underline">
+            Week 3 - Shopping List
+          </Link>
+        </li>
+
+        <li>
+          <Link href="/week-4" className="text-blue-600 hover:underline">
             Week 4 - Shopping List
           </Link>
         </li>
