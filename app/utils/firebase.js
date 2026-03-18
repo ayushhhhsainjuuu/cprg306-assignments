9cf0fc255383efd859cebdcec913508586a00204
ayush <100313112+12catsk@users.noreply.github.com>
2026-03-18 11:54:25 -0600

    final fix?

--- app/utils/firebase.js
+++ app/utils/firebase.js
@@ -6,14 +6,14 @@ const firebaseConfig = {
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId:
-    process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 let app;
 let auth;
 
+// 🚨 ONLY run in browser (THIS FIXES YOUR ERROR)
 if (typeof window !== "undefined") {
   app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
   auth = getAuth(app);
